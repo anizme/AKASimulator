0650d12eb885f1da92007a23c5cc471eb59aee2d
anizme <hirengtlesone@gmail.com>
2025-08-11 08:21:21 +0700

    refactor: prefix name changes

--- script.sh
+++ script.sh
@@ -8,16 +8,16 @@ echo "[Finish step 1] Firmware built successfully."
 
 echo
 echo "[Step 2] Build the simulator"
-# cmake -S . -B build
+cmake -S . -B build
 cmake --build build
-echo "[Finish step 2] Build complete. Check if the simulator binary is located in build/aka_simulator/"
+echo "[Finish step 2] Build complete. Check if the simulator binary is located in build/akas_simulator/"
 
 echo "[Step 3] Run the simulator"
-build/aka_simulator/stm32f103c8t6_emulator elf/firmware.elf output/emulation_log/emulation.log
+build/akas_simulator/stm32f103c8t6_emulator elf/firmware.elf akas_output/emulation_log/emulation.log
 echo "[Finish step 3] Simulator is running. You can now interact with it."
 
 echo "[Step 4] Test report genertion"
-g++ aka_test_report_generator/Generator.cpp -o aka_test_report_generator/Generator
-aka_test_report_generator/Generator output/emulation_log/code_line_emulation.log output/test_report
-echo "[Finish step 4] Test report generated successfully. Check the output/test_report directory."
+g++ akas_report_generator/Generator.cpp -o akas_report_generator/Generator
+akas_report_generator/Generator akas_output/emulation_log/code_line_emulation.log akas_output/test_report
+echo "[Finish step 4] Test report generated successfully. Check the akas_output/test_report directory."
 read -p "Press Enter to continue..."
